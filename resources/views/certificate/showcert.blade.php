2b7879fd44a9dcb0ecffe140cc299a6c5fccb202
gmirilla <gmirilla@gmail.com>
2024-03-12 11:29:30 +0100

    freight

--- resources/views/certificate/showcert.blade.php
+++ resources/views/certificate/showcert.blade.php
@@ -171,7 +171,7 @@
         text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" id="taskexporterinvoicevalue"  name="exporterinvoicevalue" {{$ro}}>
     </div>
     <div class="form-group" >
-        <label for="freightcharges"  >FREIGHT CHARGES:</label>
+        <label for="freightcharges"  style="display: none">FREIGHT CHARGES:</label>
         <input type="number" step="any" value="{{$cci->freightcharges}} " class="form-control block flex-1 border-1 bg-transparent py-1.5 pl-1 
         text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" id="taskfreightcharges"  name="freightcharges" {{$ro}}>
     </div>
